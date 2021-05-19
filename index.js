b625065bb1adf597a0be117b0621b62f3de2136e
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-05-19 15:53:15 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+(function () {
+	Object.defineProperty(Number.prototype, "times", {
+		comfigurable: false,
+		enumerable: false,
+		writable: false,
+		value: function times(predicate) {
+			for (let i = 0; i < this; i++) {
+				predicate(i);
+			}
+		},
+	});
+})();
